624a5afad3d54021fcc0ef8417f42408095e9cea
Erik Stoddard <estodd90@att.net>
2019-09-03 20:34:11 -0400

    Updates

---
+++ precache-manifest.e70662f29eb980aeb4bf3b4bc4eca374.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81336036462654bec09a0764157dd196",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "d55fdbe01571c93eedac",
+    "url": "/monsters-rolodex/static/css/main.b412d45c.chunk.css"
+  },
+  {
+    "revision": "cea528bd8a6c6ccbefab",
+    "url": "/monsters-rolodex/static/js/2.9918f956.chunk.js"
+  },
+  {
+    "revision": "d55fdbe01571c93eedac",
+    "url": "/monsters-rolodex/static/js/main.80e24d53.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
